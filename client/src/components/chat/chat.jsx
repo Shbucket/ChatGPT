794d6530dcbcf7b810c866ef3369c55436b264cc
shoamri joseph <shomarijoseph@gmail.com>
2023-04-11 19:00:59 -0400

    created message form component

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { useMultiChatLogic , MultiChatSocket, MultiChatWindow} from 'react-chat-engine-advanced'
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
 
-import CustomerHeader from '@/components/customHeader';
-
+import CustomerHeader from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 const Chat = () => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
-  "test",
-  "1234"
-
-  )
+    "test",
+    "1234"
+  );
   return (
-    <div style={{flexBasis: "100%"}}> 
-    <MultiChatSocket {...chatProps} /> 
-    <MultiChatWindow {...chatProps} 
-    style={{height: "100vh"}}
-    renderChatHeader={(chat)=> <CustomerHeader chat={chat} />}
-    />
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <CustomerHeader chat={chat} />}
+        renderMessageForm={(props) => {
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import {ChatBubbleLeftRightIcon, PhoneIcon} from '@heroicons/react/24/solid';
+import React from "react";
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
-const CustomerHeader = ({chat}) => {
+const CustomerHeader = ({ chat }) => {
+  console.log("chat.description", chat.description);
   return (
-    <div className='chat-header'>
-        <div className='flexbetween'> 
-<ChatBubbleLeftRightIcon className='icon-chat' />
-<h3 className='header-text'>{chat.title}</h3>
-        </div>
-        <div className="flexbetween">
-          <PhoneIcon className='icon-phone' />
-          <p className='header-text'>{chat.description}</p>
-        </div>
-        
+    <div className="chat-header">
+      <div className="flexbetween">
+        <ChatBubbleLeftRightIcon className="icon-chat" />
+        <h3 className="header-text">{chat.title}</h3>
+      </div>
+      <div className="flexbetween">
+        <PhoneIcon className="icon-phone" />
+        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
+          <p className="header-text">{chat.description}</p>
+        ) : (
+          <p className="header-text">no chat selected</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CustomerHeader
+export default CustomerHeader;
