2ab832a2ad8014e39344ce1cfc5b82f6951c1ac8
shoamri joseph <shomarijoseph@gmail.com>
2023-04-11 19:41:27 -0400

    openai config

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import express  from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import  { Configuration, OpenAIApi } from "openai"
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 /* CONFIGURATION  */
 dotenv.config();
 const app = express();
 app.use(express.json);
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policv: 'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policv: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 /* OPEN AI CONFIGURATION */
-
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
@@ -30,9 +30,11 @@ const completion = await openai.createCompletion({
 });
 console.log(completion.data.choices[0].text);
 
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
 /*SERVER SETUP */
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, ()=> {
-    console.log(`App listening on port http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App listening on port http://localhost:${PORT}`);
+});
