1bbc050bab55feb80b6a7cc8e1b42bfcc116fd83
shoamri joseph <shomarijoseph@gmail.com>
2023-04-11 13:05:20 -0400

    created custom header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 import {BrowserRouter, Routes, Router, Navigate, Route} from "react-router-dom";
+import Chat from "@/components/chat/chat";
 function App() {
 
   return (
