41dc425f70d085902637155fb56933c8c54c5285
shoamri joseph <shomarijoseph@gmail.com>
2023-04-11 19:23:04 -0400

    created server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express  from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import  { Configuration, OpenAIApi } from "openai"
+/* CONFIGURATION  */
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policv: 'cross-origin'}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+/* OPEN AI CONFIGURATION */
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Hello world",
+});
+console.log(completion.data.choices[0].text);
+
+
+/*SERVER SETUP */
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, ()=> {
+    console.log(`App listening on port http://localhost:${PORT}`);
+})
