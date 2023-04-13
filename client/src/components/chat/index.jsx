85d28bcb3dd91e50edc769c6d17491a38b96eaec
shoamri joseph <shomarijoseph@gmail.com>
2023-04-12 20:01:44 -0400

    created login and register function

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -8,11 +8,13 @@ import Ai from "@/components/customMessageForms/Ai"
 import Header from "@/components/customHeader";
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 import AiCode from "@/components/customMessageForms/AiCode";
-const Chat = () => {
+import AiAssist from "@/components/customMessageForms/AiAssist";
+
+const Chat = ({user, secret}) => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
-    "test",
-    "1234"
+  user,
+  secret
   );
   return (
     <div style={{ flexBasis: "100%" }}>
@@ -29,6 +31,9 @@ const Chat = () => {
              if (chatProps.chat?.title.startsWith("AiCode_")) {
                return <AiCode props={props} activeChat={chatProps.chat} />;
              }
+                  if (chatProps.chat?.title.startsWith("AiAssist_")) {
+                    return <AiAssist props={props} activeChat={chatProps.chat} />;
+                  }
           return (
             <StandardMessageForm props={props} activeChat={chatProps.chat} />
           );
